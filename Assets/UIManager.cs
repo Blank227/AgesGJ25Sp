92cc2e8207d1a24ff1e05d1a538417d8d2380b7e
Blank227 <august@peterstrand.se>
2025-04-05 16:43:12 +0200

    ui

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -1,18 +1,22 @@
 using TMPro;
 using UnityEngine;
+using System;
 
 public class UIManager : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI timer;
     [SerializeField] TextMeshProUGUI score;
+    int minute = 2;
+    int seconds = 9;
     void Start()
     {
+        
         score.text = $"{0}";
     }
 
     
     void Update()
     {
-        
+        timer.text = minute + ":" + seconds;
     }
 }
